da0952c1c4d5d813669d72793b813fe131a40368
Svetlana <ssv-71@mail.ru>
2022-03-29 21:39:48 +0300

    Сделан метод compare и пересыпали фрукты в другую коробку

--- src/main/java/lesson3/hw3/Apple.java
+++ src/main/java/lesson3/hw3/Apple.java
@@ -1,5 +1,8 @@
 package lesson3.hw3;
 
-public class Apple extends Fruit{
+public class Apple extends Fruit {
 
+    public Apple() {
+        super(1.0f);
+    }
 }
--- src/main/java/lesson3/hw3/Box.java
+++ src/main/java/lesson3/hw3/Box.java
@@ -1,53 +1,82 @@
 package lesson3.hw3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Box<T extends Fruit> {
-    T[] fruits;
+    ArrayList<T> fruits;
 
-    public Box(T[] items) {
-        this.fruits = items;
+    public Box(List<T> fruits) {
+        this.fruits = new ArrayList<>(fruits);
     }
 
-    public T[] getFruits() {
+    public ArrayList<T> getFruits() {
         return fruits;
     }
 
-    public void setFruits(T[] fruits) {
-        this.fruits = fruits;
+    public void setFruits(List<T> fruits) {
+        this.fruits = new ArrayList<>(fruits);
     }
 
-    public double getWeight(){
-       double weight;
-        if (fruits[0] instanceof Apple) {
+    public float getWeight(){
+       float weight = 0.0f;
+        if (fruits.get(0) instanceof Apple) {
             weight = 1.0f;
         } else
-            if(fruits[0] instanceof Orange) {
+            if(fruits.get(0) instanceof Orange) {
             weight = 1.5f;
         }else
             throw new RuntimeException("Относится к другому типу фруктов");
-        return weight * fruits.length;
+        return weight * fruits.size();
     }
 
     @Override
     public String toString() {
-        if (fruits[0] instanceof Apple){
-            return "Коробка с яблоками";
+        if (fruits.size() == 0) {
+            return "пустая коробка";
+        } else
+        if (fruits.get(0) instanceof Apple){
+            return "коробка с яблоками( " + fruits.size() + " шт)";
         }else {
-            return "Коробка с апельсинами";
+            return "коробка с апельсинами( " + fruits.size() + " шт)";
         }
     }
 
+    public boolean compare(Box<?> box) {
+        return getWeight() == box.getWeight();
+    }
+
+    public void sprinkle(Box<T> anotherBox){
+        anotherBox.fruits.addAll(this.fruits);
+        this.fruits.clear();
+    }
+
+
     public static void main(String[] args) {
 
-       Box<Apple> appleBox = new Box<>(new Apple[] {new Apple(), new Apple()});
+        Box<Apple> appleBox = new Box<Apple>( Arrays.asList(new Apple(), new Apple()));
+        System.out.println("Вес " + appleBox + " " + appleBox.getWeight());
 
-       System.out.println("Вес " + appleBox + " " + appleBox.getWeight());
-
-
-        Box<Orange> orangeBox = new Box<>(new Orange[]{new Orange(), new Orange(), new Orange()});
+        Box<Orange> orangeBox = new Box<Orange>(Arrays.asList(new Orange(), new Orange(), new Orange()));
         System.out.println("Вес " + orangeBox + " " + orangeBox.getWeight());
 
 
+        System.out.println(appleBox.compare(orangeBox));
+
+        // как создать переменную:
+        // ТИП название ( = )
+
+        Box<Apple> anotherBox = new Box<>(Arrays.asList(new Apple(), new Apple()));
+        System.out.println(appleBox + ", " + anotherBox);
+        appleBox.sprinkle(anotherBox);
+        System.out.println(appleBox + ", " + anotherBox);
+//        Box box = new Box(Arrays.asList());
+//        System.out.println(box.количествоФруктов);
+//        box.добавитьФруктов(5);
+//        System.out.println(box.количествоФруктов);
     }
+
+
+
 }
